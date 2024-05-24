0193ba87f4497fb793130dafaa90c26cf78a0829
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-05-24 18:12:24 +0200

    maak de spaties in download_video functie correct

--- functies.py
+++ functies.py
@@ -3,13 +3,13 @@ from pytube import YouTube
 def download_video(link:str,opitie:str,file_extension:str):
   yt = YouTube(link)
   if opitie == 'highest_resolution':
-      video = yt.streams.get_highest_resolution()
+    video = yt.streams.get_highest_resolution()
   elif opitie == 'audio_only':
     video = yt.streams.get_audio_only()
   elif opitie == 'lowest_resolution':
-      video = yt.streams.get_lowest_resolution()
+    video = yt.streams.get_lowest_resolution()
   else:
-      print('')
+    print('')
   video.download(file_extension=file_extension)
 
 def add_to_queue(download_queue:list):
