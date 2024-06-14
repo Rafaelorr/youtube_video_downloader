6b2a2860bddee292b2f7b4bb40baf4eb8a855fbe
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-06-14 14:16:41 +0200

    verander download_queue variable naar download_queue_list

--- functies.py
+++ functies.py
@@ -12,31 +12,31 @@ def download_video(link:str,opitie:str,file_extension:str) -> None:
     print('')
   video.download(file_extension=file_extension)
 
-def add_to_queue(download_queue:list) -> list:
+def add_to_queue(download_queue_list:list) -> list:
   link:str = input("video link: ")
   opitie:str = input("opitie highest_resolution, audio_only of lowest_resolution: ")
   file_extension:str = input("video file extension: ")
-  download_queue.append(link)
-  download_queue.append(opitie)
-  download_queue.append(file_extension)
-  print("nieuwe video toegevoegd aan download_queue")
-  return download_queue
+  download_queue_list.append(link)
+  download_queue_list.append(opitie)
+  download_queue_list.append(file_extension)
+  print("nieuwe video toegevoegd aan download_queue_list")
+  return download_queue_list
 
-def list_queue(download_queue:list) -> None:
-  for item in download_queue:
-    if download_queue.index(item) % 4 == 0 or download_queue.index(item) == 0:
+def list_queue(download_queue_list:list) -> None:
+  for item in download_queue_list:
+    if download_queue_list.index(item) % 4 == 0 or download_queue_list.index(item) == 0:
       print("video link: ",item)
       continue
-    if download_queue.index(item) % 2 == 0:
+    if download_queue_list.index(item) % 2 == 0:
       print("video download opitie: ", item)
       continue
-    if download_queue.index(item) % 3 == 0:
+    if download_queue_list.index(item) % 3 == 0:
       print("video file extension: ", item)
       continue
 
-def download_queue(download_queue:list) -> None:
-  for i in range(len(download_queue/3)):
-    link:str = download_queue[i]
-    opitie:str = download_queue[i+1]
-    file_extension:str = download_queue[i+2]
+def download_queue(download_queue_list:list) -> None:
+  for i in range(len(download_queue_list/3)):
+    link:str = download_queue_list[i]
+    opitie:str = download_queue_list[i+1]
+    file_extension:str = download_queue_list[i+2]
     download_video(link=link,opitie=opitie,file_extension=file_extension)
