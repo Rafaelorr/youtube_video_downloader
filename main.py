b016ccef132de532035accbfb2e956b10b79b84e
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-10-19 10:39:53 +0200

    voeg alle functies toe als command in main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pytubefix import YouTube, Playlist, Channel
 from audio_functies import download_audio,download_channel_audio,download_audio_queue,download_playlist_audio
 from video_functies import download_video, download_video_queue, download_channel_video, download_playlist_video
 import os
@@ -15,25 +14,46 @@ def list_queue(download_queue_list:list) -> None:
 
 def help():
   print("""
-   add_to_queue: voegt video toe aan de download_queue
-   download_video_queue: download de queue,
+   add_to_queue: voegt video toe aan de download_queue, \n
+   download_video_queue: download de queue als video, \n
+   download_audio_queue: download de queue als audio, \n
    list_queue: toont de queue als een python list,
-   exit: om het programma te stoppen WAARSCHUWING je download_queue blijf niet bewaart als je het programma stop,
-   clear: maak de command line leeg,
-   download_playlist_video: download een volledige playlist,
-   download_channel_video: download een volledig youtube kanaal als videos,
-   help: lijst van alle commands en hun functie.
+   exit: om het programma te stoppen \n WAARSCHUWING je download_queue blijf niet bewaart als je het programma stop, \n
+   clear: maak de command line leeg, \n
+   download_playlist_video: download een volledige playlist, \n
+   download_channel_video: download een volledig youtube kanaal als videos, \n
+   download_playlist_audio: download een volledige playlist als audio, \n
+   download_channel_audio: download een volledig youtube kanaal als audio, \n
+   download_audio: download een video als audio, \n
+   download_video: download een video als video, \n
+   help: lijst van alle commands en hun functie. \n
   """) 
 
 download_queue_list:list = []
 
+print("Typ help voor een lijst van de commands")
+
 # user command loop
 while True:
   command:str = input("command: ")
+
   if command == "add_to_queue":
-    add_to_queue(download_queue_list=download_queue_list)
+    add_to_queue(download_queue_list)
+
   elif command == "download_video_queue":
-    download_video_queue(download_queue_list=download_queue_list)
+    download_video_queue(download_queue_list)
+  
+  elif command == "download_audio_queue":
+    download_audio_queue(download_queue_list)
+
+  elif command == "download_audio":
+    link :str = input("video link: ")
+    download_audio(link)
+  
+  elif command == "download_video":
+    link :str = input("video link: ")
+    download_video(link)
+
   elif command == "exit":
     want_exit:str = input("wil je echt stoppen? y/n ")
     while not want_exit == "y" and not want_exit == "n":
@@ -44,12 +64,23 @@ while True:
     continue
   elif command == "list_queue":
     list_queue(download_queue_list=download_queue_list)
+
   elif command == "download_playlist_video":
     playlist_link :str = input("playlist link: ")
     download_playlist_video(playlist_link)
+
   elif command == "download_channel_video":
     channel_link :str = input("channel link: ")
     download_channel_video(channel_link)
+
+  elif command == "download_playlist_audio":
+    playlist_link :str = input("playlist link: ")
+    download_playlist_audio(playlist_link)
+
+  elif command == "download_channel_audio":
+    channel_link :str = input("channel link: ")
+    download_channel_audio(channel_link)
+
   elif command == "clear":
     os.system('cls' if os.name == 'nt' else 'clear')
   else:
