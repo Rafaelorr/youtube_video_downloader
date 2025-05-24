3bdf3f2780064934dce15cabbc0cde3d9b9e8361
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-05-24 12:28:59 +0200

    port de audio_functies naar yt_dlp

--- audio_functies.py
+++ audio_functies.py
@@ -1,13 +1,41 @@
 import yt_dlp
 
 def download_audio(link:str) -> None:
-  pass
+  ydl_opts = {
+    'extract_audio': True,
+    'format': 'bestaudio/best[height<=1080]',
+    'outtmpl': 'downloads/%(title)s.mp3'
+  }
+  with yt_dlp.YoutubeDL(ydl_opts) as video:
+    info_dict = video.extract_info(link, download = True)
+    video_title = info_dict['title']
+    print(video_title)
+    video.download(link)
 
 def download_channel_audio(channel_link:str) -> None:
-  pass
+  ydl_opts = {
+    'extract_audio': True,
+    'format': 'bestaudio/best[height<=1080]',
+    'outtmpl': 'downloads/%(title)s.mp3',
+    'ignoreerrors': True,
+    'extract_flat': False,
+    'playlistend': None,
+    'noplaylist': False,
+    'playliststart': 1,
+  }
 
-def download_audio_queue(download_queue_list:list) -> None:
-  pass
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([channel_link])
+
+def download_audio_queue(download_queue_list:list[str]) -> None:
+  for video in download_queue_list:
+    download_audio(video)
 
 def download_playlist_audio(playlist_link:str) -> None:
-  pass
+  ydl_opts = {
+    'extract_audio': True,
+    'format': 'bestaudio/best[height<=1080]',
+    'outtmpl': 'downloads/%(playlist)s/%(playlist_index)s - %(title)s.mp3',
+  }
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([playlist_link])
