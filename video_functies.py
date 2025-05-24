6629e1bc7935cca067fff89a8d13c0630394dc7f
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-05-24 11:53:41 +0200

    voeg logica toe voor niet beschikbare videos in video_functies.py

--- video_functies.py
+++ video_functies.py
@@ -4,16 +4,19 @@ def download_channel_video(channel_link:str) -> None:
 
   ydl_opts = {
     'ignoreerrors': True,
-    'extract_flat': False,  # Set to True if you want only metadata without downloading
-    'playlistend': None,    # Download all videos
-    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Output directory and filename pattern
+    'extract_flat': False,
+    'playlistend': None,
+    'outtmpl': 'downloads/%(title)s.%(ext)s',
     'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
-    'noplaylist': False, 
+    'noplaylist': False,
     'playliststart': 1,
   }
 
   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([channel_link])
+    try:
+      ydl.download([channel_link])
+    except yt_dlp.utils.DownloadError:
+      print("Niet beschikbaar")
 
 def download_playlist_video(playlist_link:str) -> None:
   ydl_opts = {
@@ -21,14 +24,20 @@ def download_playlist_video(playlist_link:str) -> None:
     'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
   }
   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([playlist_link])
+    try:
+      ydl.download([playlist_link])
+    except yt_dlp.utils.DownloadError:
+      print("Niet beschikbaar")
 
 
 def download_video(link:str) -> None:
   ydl_opts = {'outtmpl': 'downloads/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
               'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]'}
   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+    try:
+      ydl.download([link])
+    except yt_dlp.utils.DownloadError:
+      print("Niet beschikbaar")
 
 def download_video_queue(download_queue_list:list[str]) -> None:
   for video in download_queue_list:
