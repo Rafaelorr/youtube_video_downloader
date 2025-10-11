09e6fbbcd7a79e45ea78d4c20f5ba2930078b540
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-10-11 20:27:41 +0200

    verbeter tekst in main functies

--- src/cli/main_functies.py
+++ src/cli/main_functies.py
@@ -1,7 +1,7 @@
 def add_to_queue(download_queue_list:list) -> list:
-  link:str = input("video link: ")
+  link:str = input("Video link: ")
   download_queue_list.append(link)
-  print("nieuwe video toegevoegd aan download_queue_list")
+  print("Nieuwe video toegevoegd aan de download queue.")
   return download_queue_list
 
 def list_queue(download_queue_list:list) -> None:
@@ -9,18 +9,17 @@ def list_queue(download_queue_list:list) -> None:
 
 def help():
   print("""
-   add_to_queue: voegt video toe aan de download_queue,
-   download_video_queue: download de queue als video,
-   download_audio_queue: download de queue als audio,
-   list_queue: toont de queue als een python list,
-   exit: om het programma te stoppen 
-   WAARSCHUWING je download_queue blijf niet bewaart als je het programma stop,
-   clear: maak de command line leeg, 
-   download_playlist_video: download een volledige playlist, 
-   download_channel_video: download een volledig youtube kanaal als videos, 
-   download_playlist_audio: download een volledige playlist als audio, 
-   download_channel_audio: download een volledig youtube kanaal als audio, 
-   download_audio: download een video als audio, 
-   download_video: download een video als video, 
-   help: lijst van alle commands en hun functie. 
-  """) 
+   add_to_queue: voegt een video toe aan de download_queue,
+   download_video_queue: downloadt de queue,
+   download_audio_queue: downloadt de queue als audio,
+   list_queue: toont de download_queue,
+   exit: stopt het programma 
+   WAARSCHUWING: de download_queue blijft niet bewaart als je het programma stopt,
+   clear: maakt de command line leeg, 
+   download_playlist_video: downloadt een playlist, 
+   download_channel_video: downloadt een youtube kanaal, 
+   download_playlist_audio: downloadt een playlist als audio, 
+   download_channel_audio: downloadt een youtube kanaal als audio, 
+   download_audio: downloadt een video als audio, 
+   download_video: downloadt een video, 
+   help: geeft een lijst van alle commands en hun functie. """) 
