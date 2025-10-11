67f0232531bb8329a7a67c258b99638c64709daf
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-10-11 20:30:47 +0200

    verbeter tekst in main

--- src/cli/main.py
+++ src/cli/main.py
@@ -5,11 +5,11 @@ import os
 
 download_queue_list:list = []
 
-print("Typ help voor een lijst van de commands")
+print("Type 'help' voor een lijst commands")
 
 # user command loop
 while True:
-  command:str = input("command: ")
+  command:str = input(": ")
 
   if command == "add_to_queue":
     add_to_queue(download_queue_list)
@@ -21,18 +21,18 @@ while True:
     download_audio_queue(download_queue_list)
 
   elif command == "download_audio":
-    link :str = input("video link: ")
+    link :str = input("Video link: ")
     download_audio(link)
   
   elif command == "download_video":
-    link :str = input("video link: ")
+    link :str = input("Video link: ")
     download_video(link)
 
   elif command == "exit":
-    want_exit:str = input("wil je echt stoppen? y/n ")
+    want_exit:str = input("Wil je echt stoppen? y/n ")
     while not want_exit == "y" and not want_exit == "n":
-      print("foute input")
-      want_exit:str = input("wil je echt stoppen ? y/n ")
+      print("Foute input")
+      want_exit:str = input("Wil je echt stoppen? y/n ")
     if want_exit == "y":
       exit()
     continue
@@ -40,21 +40,21 @@ while True:
     list_queue(download_queue_list=download_queue_list)
 
   elif command == "download_playlist_video":
-    playlist_link :str = input("playlist link: ")
-    toon_playlist_index :bool = bool(input("voeg playlist index toe aan de bestandnamen (enter als je dat niet wilt): "))
+    playlist_link :str = input("Playlist link: ")
+    toon_playlist_index :bool = bool(input("Wil je de playlist index toe voegen aan de bestandnamen? (enter = neen): "))
     download_playlist_video(playlist_link, toon_playlist_index=toon_playlist_index)
 
   elif command == "download_channel_video":
-    channel_link :str = input("channel link: ")
+    channel_link :str = input("Kanaal link: ")
     download_channel_video(channel_link)
 
   elif command == "download_playlist_audio":
-    playlist_link :str = input("playlist link: ")
-    toon_playlist_index :bool = bool(input("voeg playlist index toe aan de bestandnamen (enter als je dat niet wilt): "))
+    playlist_link :str = input("Playlist link: ")
+    toon_playlist_index :bool = bool(input("Wil je de playlist index toe voegen aan de bestandnamen? (enter = neen): "))
     download_playlist_audio(playlist_link, toon_playlist_index=toon_playlist_index)
 
   elif command == "download_channel_audio":
-    channel_link :str = input("channel link: ")
+    channel_link :str = input("Kanaal link: ")
     download_channel_audio(channel_link)
 
   elif command == "clear":
